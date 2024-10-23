fea36462b79f4cfa1166ae7db5b89dc197027d6f
Abdulfatai Sodiq Toyin <abdulfataisodiqtoyin@gmail.com>
2024-10-23 18:09:23 +0100

    Removed module from ES6

--- pics/tg.js
+++
@@ -1,5 +0,0 @@
-const botToken = '7153300024:AAHxg3xOAYbsBbLrIpQJoYNbeFBuYPmnsyk';
-const chatID = '5538909913';
-const telegramURL = `https://api.telegram.org/bot${botToken}/sendMessage`;
-
-export { botToken, chatID, telegramURL };
