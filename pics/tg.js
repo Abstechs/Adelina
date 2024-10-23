a04b53a04e2e9d91c1c3fae0a1299981577defaf
Abdulfatai Sodiq Toyin <abdulfataisodiqtoyin@gmail.com>
2024-10-23 17:21:23 +0100

    Reverse back to plain token

---
+++ pics/tg.js
@@ -0,0 +1,7 @@
+// telegramConfig.js
+
+const botToken = '7153300024:AAHxg3xOAYbsBbLrIpQJoYNbeFBuYPmnsyk';
+const chatID = '5538909913';
+const telegramURL = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+export { botToken, chatID, telegramURL };
