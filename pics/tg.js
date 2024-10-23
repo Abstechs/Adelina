9c33f5301e1067ddfafcc09091be4a7563c6e1ee
Abdulfatai Sodiq Toyin <abdulfataisodiqtoyin@gmail.com>
2024-10-23 17:52:01 +0100

    Fixed module import/export in ES6

--- pics/tg.js
+++ pics/tg.js
@@ -1,5 +1,3 @@
-// telegramConfig.js
-
 const botToken = '7153300024:AAHxg3xOAYbsBbLrIpQJoYNbeFBuYPmnsyk';
 const chatID = '5538909913';
 const telegramURL = `https://api.telegram.org/bot${botToken}/sendMessage`;
